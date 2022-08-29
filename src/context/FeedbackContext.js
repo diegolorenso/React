023e4fecd1efba38e66ebdd17c6136f9b17083b0
Diego Lorenso <diego@lorenso.com>
2022-08-29 15:59:44 -0500

    initial commit

---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,79 @@
+import { v4 as uuidv4 } from "uuid";
+import { createContext, useState } from "react";
+
+const FeedbackContext = createContext();
+
+export const FeedbackProvider = ({ children }) => {
+  const [feedback, setFeedback] = useState([
+    {
+      id: 1,
+      text: "This item is from context",
+      rating: 10,
+    },
+    {
+      id: 2,
+      text: "This item",
+      rating: 9,
+    },
+    {
+      id: 3,
+      text: "This item is context",
+      rating: 7,
+    },
+  ]);
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+
+  //Add Feedback
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
+  //Update feedback item
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+      feedback.map((item) =>
+        item.id === id
+          ? {
+              ...item,
+              ...updItem,
+            }
+          : item
+      )
+    );
+  };
+
+  //Delete Feedback
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  //Set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
+
+  return (
+    <FeedbackContext.Provider
+      value={{
+        updateFeedback,
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+        feedbackEdit,
+        feedback,
+      }}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  );
+};
+export default FeedbackContext;
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,23 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "aserjkakl;sefjaskdjf as;kldjfa asdfas;dkfjas;kdfjaskdfasfritoqjhwerti qjas",
+  },
+  {
+    id: 2,
+    rating: 3,
+    text: "aserjkakl;sefjaskdjf as;kldjfa asdfas;dkfjas;kdfjaskdfasfritoqjhwerti qjas",
+  },
+  {
+    id: 3,
+    rating: 7,
+    text: "aserjkakl;sefjaskdjf as;kldjfa asdfas;dkfjas;kdfjaskdfasfritoqjhwerti qjas",
+  },
+  {
+    id: 4,
+    rating: 8,
+    text: "aserjkakl;sefjaskdjf as;kldjfa asdfas;dkfjas;kdfjaskdfasfritoqjhwerti qjas",
+  },
+];
+export default FeedbackData;
